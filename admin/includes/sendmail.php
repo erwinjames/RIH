0cd3f6b748fac00021428649fb67e247a6af1ab5
erwinjames <manugasewinjames@gmail.com>
2020-12-30 08:29:47 +0800

    initail commit

--- admin/includes/sendmail.php
+++ admin/includes/sendmail.php
@@ -1,39 +1,85 @@
 <?php
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+
+// require 'E:\xampp\composer\vendor\autoload.php';
+// require 'PHPMailer\src\Exception.php';
+// require 'PHPMailer\src\PHPMailer.php';
+// require 'PHPMailer\src\SMTP.php';
+// //PHPMailer Object
+// $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+
+// //From email address and name
+// $mail->From = "manugasewinjames@gmail.com";
+// $mail->FromName = "Erwin James Manugas";
+
+// //To address and name
+// $mail->addAddress("manugasewinjames@gmail.com", "Your Name");
+// $mail->addAddress("manugasewinjames@gmail.com"); //Recipient name is optional
+
+// //Address to which recipient will reply
+// $mail->addReplyTo("manugasewinjames@gmail.com", "Reply");
+
+// //CC and BCC
+// $mail->addCC("cc@example.com");
+// $mail->addBCC("bcc@example.com");
+
+// //Send HTML or Plain Text email
+// $mail->isHTML(true);
+
+// $mail->Subject = "Subject Text";
+// $mail->Body = "<i>Mail body in HTML</i>";
+// $mail->AltBody = "This is the plain text version of the email content";
+
+// try {
+//     $mail->send();
+//     echo "Message has been sent successfully";
+// } catch (Exception $e) {
+//     echo "Mailer Error: " . $mail->ErrorInfo;
+// } 
+
+require 'PHPMailer\src\PHPMailer.php';
+require 'E:\xampp\composer\vendor\autoload.php';
+require 'PHPMailer\src\Exception.php';
+//include('class.phpmailer.php');
+//require("class.smtp.php");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+	
+		$name="ej";
+		$email="manugasewinjames@gmail.com";
+		$phone="ambot";
+		$message="okay nice world";
 
-require_once "vendor/autoload.php";
+$mail = new PHPMailer;
+$mail-> isSMTP(); # SMTP is enabled now.
+$mail-> Host = 'smtp.gmail.com'; //smtp.gmail.com
+$mail->SMTPDebug   = 2;
+$mail-> SMTPAuth = true;
+//$mail->SMTPDebug = 3;
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+//$admin_email="proyotech@gmail.com";
 
-//From email address and name
-$mail->From = "from@yourdomain.com";
-$mail->FromName = "Full Name";
+$mail->setFrom($email, $name);
+$mail->addAddress("svkdatt@gmail.com", 'Proyo Technologies');     // Add a recipient
 
-//To address and name
-$mail->addAddress("recepient1@example.com", "Recepient Name");
-$mail->addAddress("recepient1@example.com"); //Recipient name is optional
 
-//Address to which recipient will reply
-$mail->addReplyTo("reply@yourdomain.com", "Reply");
+$mail->addReplyTo($email, $name);
 
-//CC and BCC
-$mail->addCC("cc@example.com");
-$mail->addBCC("bcc@example.com");
+$mail->isHTML(true);                                  // Set email format to HTML
 
-//Send HTML or Plain Text email
-$mail->isHTML(true);
 
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+$mail->Subject = 'this is subject part';
+$mail->Body    = "<strong>".$name."</strong>"."<br>".$phone."<br>".$email."<br>".$message;
+$mail->AltBody = 'nice job';
 
-try {
-    $mail->send();
-    echo "Message has been sent successfully";
-} catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} 
+if(!$mail->send()) {
+    echo 'Message could not be sent. ';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+ echo "yes";
+exit();
+}
+
 
 ?>
